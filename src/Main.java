59c944c5469a9baa00035ff49ddb65c882a0a768
jadyms <33762825+jadyms@users.noreply.github.com>
2020-03-05 21:34:55 +0000

    2.Create Country Class and Continent Enum

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,10 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,5 +16,31 @@
  * @author JadyMartins
  */
 public class Main {
+
+    public Main() {
+        
+        System.out.println("INSERT A NUMBER OPTION");
+        System.out.println("1 - Retrieve all records");
+        System.out.println("2 - Retrieve records by country name");
+        System.out.println("3 - Retrieve records by country code");
+        System.out.println("4 - Add new records to the database");
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        String input = "";
+        
+        try {
+            input = br.readLine();
+        } catch (Exception e) {
+            System.out.println("Insert an option1");
+        }
+    }
+
+    
+       public static void main (String [] args){
+        
+        new Main();
+    
+    }
     
 }
