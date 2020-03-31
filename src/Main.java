5e12352ccb8e6d8acebb22ab7df7b5a471626b20
jadyms <33762825+jadyms@users.noreply.github.com>
2020-03-31 17:49:34 +0100

    22.Commenting Part 2

--- src/Main.java
+++ src/Main.java
@@ -7,149 +7,135 @@ import java.util.ArrayList;
 
 //Client class of the program
 public class Main{
-    CountryDAO dao = new MySQLCountryDAO();
-    Menu init      = Menu.getInstance();
-    Input input    = new Input();
+    CountryDAO dao = new MySQLCountryDAO(); //connection to the db using DAO
+    Menu init = Menu.getInstance(); //prints data to console
+    InputReader inputReader = new InputReader(); //get and validate inputReader
+    String input = null; //for any user input
             
-
     public Main() throws IOException {
         
         //Print welcome message
         init.printMessage("SELECT AN OPTION FROM THE MENU");
-        
-        String option;
+
         do{
             //Print menu of options
-             init.printMenu();
-             //Read user input 
-             option = input.ReadInput();
+            init.printMenu();
+            
+            //Read user inputReader 
+            input = inputReader.bufferedReader();
                          
-             if (option.matches("1")) {
-                //Retrieve all records stored in the database table
+             //Retrieve all records stored in the database table
+            if(input.matches("1")){
+                //Create arrayList of countries
                 ArrayList<Country> countries = dao.getCountries();
-                
+                //Print the arrayList
                 for(Country c: countries){
                     System.out.println(c);
                 }
+                //End of transaction
                 System.out.println("=====  Press ENTER to continue =====");
                 System.in.read();
              
-             } else if (option.equals("2")) {
-                //Retrieve records by country name
-                String message = "Input country name: ";
-                 option = input.getInput(message);
+            //Retrieve records by country name  
+            }else if(input.equals("2")){
                 
-                ArrayList<Country> countries = dao.findByName(option);
+                //Get the country name
+                String message = "Input country name: ";
+                input = inputReader.getInput(message);
+                
+                //Look for country given the name
+                //Return countries in a arrayList
+                ArrayList<Country> countries = dao.findByName(input);
+                
+                //If name not found, letthe user know
                 if(countries.isEmpty()){
-                    System.out.println(option + " not found in the database");
-                }                 
+                    System.out.println(input + " not found in the database");
+                }
+                //If found, print all the countries
                 for(Country c: countries){
                     System.out.println(c);
                 }
-                
+                //End of transaction
                 System.out.println("====  Press ENTER to continue =====");
                 System.in.read();
            
-             }else if (option.equals("3")) {
-                //Retrieve records by country name
-                                 
+            //Retrieve records by country name
+            }else if(input.equals("3")){
+                
+                 //Get the country code
                 String message = "Please input a country code: ";
+                input = inputReader.getInput(message);
                 
-                option = input.getInput(message);
-                
-                
-                Country country = dao.findByCode(option);
+                //Look for country given the code and return it
+                Country country = dao.findByCode(input);
+                //if country not found, let the user know
                 if(country == null){
-                    System.out.println(option + " not found in the database");
-                } else{
-                
-                System.out.println("Country with code "+  option + " : " +country);
-
+                    System.out.println(input + " not found in the database");
+                //print country to the user    
+                }else{
+                    System.out.println("Country with code "+  input + " : " +country);
                 }
-                                 
-                                
+                //End of Transaction
                 System.out.println("====  Press ENTER to continue =====");
                 System.in.read();
            
-             }else if (option.equals("4")) {
-                   //Add new records into the database
-                   
-                   //Generate code
-                   
-                   // send code to db and get a boolean
-                   
-                   //send boolean to 
-                   // String code = input.getValidCode("Input country code: ");
-                    Country country;
-                     String randomCode;
-                    do{
-                     randomCode = input.randomCode(3);
-                        System.out.println(randomCode);
-                    
+                //Add new records into the database
+            }else if (input.equals("4")) {
+                //Create a random 3char code and check if it exists in the db
+                String randomCode;   
+                Country country = null; 
+                do{
+                    randomCode = inputReader.randomCode(3); //create a code
                     //Check if code already exists
                     country = dao.findByCode(randomCode);
-                    
-                                 
-                    }while(country != null);
-                    String code = randomCode;
-                    System.out.println("Country code: "+ randomCode);
-                    
-                    String name = input.getAlphabeticName("Input country name: ");
-                    
-                    
-                    String continent = input.getValidContinent("Input a continent from the options: \n" 
-                            + Continent.getContinents());
-                             
-               
-                    String surfaceAreaString = input.getValidSurfaceArea("Input surface area: ");
-                    double surfaceArea = Double.valueOf(surfaceAreaString);
-                    
-                    String headOfState = input.getAlphabeticName("Input head of state: ");            
-                   
-                    
-                   Country newCountry = new Country.CountryBuilder(code, name, headOfState).setContinent(continent).setSurfaceArea(surfaceArea).build();
-          
-                  
+                }while(country != null);//repeat while no country code is found
                 
-                 
-                        boolean isInserted = dao.insertCountry(newCountry);
-                        if(isInserted){
-                               System.out.println("Country "  + newCountry + " created");
-                      
-                        }
-                
-                      
-                    
-                 
- System.out.println("====  Press ENTER to continue =====");
+                //Assign randomCode to String code
+                String code = randomCode;
+                System.out.println("Country code: "+ randomCode);
+                //Get country name
+                String name = inputReader.getAlphabeticName(
+                        "Input country name: ");
+                //Get continent
+                String continent = inputReader.getValidContinent(
+                        "Input a continent from the options: \n" 
+                        + Continent.getContinents());
+                //Get surface Area and parse it to double
+                String surfaceAreaString = inputReader.getValidSurfaceArea(
+                        "Input surface area: ");
+                double surfaceArea = Double.valueOf(surfaceAreaString);
+                //Get head of state
+                String headOfState = inputReader.getAlphabeticName(
+                        "Input head of state: ");   
+                //Create the country using Builder
+                Country newCountry = new Country.CountryBuilder(code, name, headOfState)
+                        .setContinent(continent)
+                        .setSurfaceArea(surfaceArea)
+                        .build();
+                //Insert into the db and return true if successful
+                boolean isInserted = dao.insertCountry(newCountry);
+                //If inserted into the database
+                if(isInserted){
+                    System.out.println("Country "  + newCountry + " created");
+                }
+                //End of Transaction
+                System.out.println("====  Press ENTER to continue =====");
                 System.in.read();
-		}else if (option.equals("5")){
-                    
-                init.printMessage("THANK YOU. GOODBYE");
-               return;
-	}
                 
-             
-             
-              
-              
-              
-            
-        }while(!(option.equals("")) || !(option.length() == 1) || !(option.matches("[1-5]+")));
-
-  
-        
-    
-        
-
+                //Terminate the system
+		}else if (input.equals("5")){
+                //Print goodbye message    
+                init.printMessage("THANK YOU. GOODBYE");
+                //Terminate
+                return;
+	}
+            //until input matches 1 to 5
+        }while(!(input.equals("")) || 
+               !(input.length() == 1) || 
+               !(input.matches("[1-5]+")));
     }
-    
-   
-  
-       public static void main (String [] args) throws IOException{
+
+    public static void main (String [] args) throws IOException{
         new Main();
-       
-    
     }
-    
 }
